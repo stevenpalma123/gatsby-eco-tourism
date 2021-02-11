9ae6211e623235b6d2408dd5e94f14bbb08ecf39
David Sapp <david@mobius.digital>
2021-02-11 09:28:57 -0700

    update config

--- config/site.js
+++ config/site.js
@@ -1,26 +1,26 @@
 module.exports = {
-  pathPrefix: '/', // Prefix for all links. If you deploy your site to example.com/portfolio your pathPrefix should be "portfolio"
-  title: 'Example Company', // Navigation and Site Title
-  titleAlt: 'Example Company', // Title for JSONLD
-  description: 'Example Company',
-  url: 'https://example.com', // Domain of your site. No trailing slash!
-  siteUrl: 'https://example.com', // url + pathPrefix
+  pathPrefix: '/', // Prefix for all links. If you deploy your site to ecotourismbelize.com/portfolio your pathPrefix should be "portfolio"
+  title: 'EcoTourism Belize', // Navigation and Site Title
+  titleAlt: 'EcoTourism Belize', // Title for JSONLD
+  description: 'EcoTourism Belize',
+  url: 'https://ecotourismbelize.com', // Domain of your site. No trailing slash!
+  siteUrl: 'https://ecotourismbelize.com', // url + pathPrefix
   blogUri: '/blog',
   siteLanguage: 'en', // Language Tag on <html> element
   logo: 'src/assets/images/logo.png', // Used for SEO
   banner: 'src/assets/images/banner.png',
   // JSONLD / Manifest
   favicon: 'src/assets/images/favicon.png', // Used for manifest favicon generation
-  shortName: 'example', // shortname for manifest. MUST be shorter than 12 characters
-  author: 'Example Author', // Author for schemaORGJSONLD
+  shortName: 'etb', // shortname for manifest. MUST be shorter than 12 characters
+  author: 'David Sapp', // Author for schemaORGJSONLD
   themeColor: '#ff6363',
   backgroundColor: '#ff6363',
   twitter: '', // Twitter Username
   // EXTRA
   hoursOfOperation: 'M-F: 9am - 5pm',
-  address: '123 Example St, Exampletown, USA',
-  phone: '123.123.1234',
-  email: 'me@example.com',
+  address: 'Ogaldez Street, Punta Gorda Town, Toledo District, Belize',
+  phone: '(+501)-722-0108',
+  email: 'info@ecotourismbelize.com',
   // FOOTER
-  company: 'Example Company',
+  company: 'EcoTourism Belize',
 };
